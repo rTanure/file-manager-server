af120fb1432000597ac3ad2fa85ef5a5ec21a9ff
unknown <richardy.tanure@hotmail.com>
2023-09-17 03:19:36 -0300

    change create file controller

---
+++ client/src/utils/services.js
@@ -0,0 +1,1 @@
+const baseUrl = "http://localhost:3333/api/text/create"
--- server/src/controllers/textControllers.js
+++ server/src/controllers/textControllers.js
@@ -1,6 +1,9 @@
 const z = require("zod")
+const fs = require("fs")
+const path = require("path")
+const temp_path = __dirname
 
-function createTextFile(req, res) {
+async function createTextFile(req, res) {
   const bodySchema = z.object({
     title: z.string(),
     content: z.string()
@@ -8,7 +11,20 @@ function createTextFile(req, res) {
 
   const { title, content } = bodySchema.parse(req.body)
 
-  res.send({title, content})
+  const rootPath = path.resolve(path.join(__dirname), "../../")
+  const filePath = path.join(`${rootPath}/temp/${title}.txt`)
+
+  fs.writeFile(filePath, content, () => {
+    res.sendFile(filePath)
+  })
+
+
+
+  setTimeout(() => {
+    fs.unlink(filePath, ()=>console.log("Arquivo deletado"))
+  }, 3000);
+
+  
 }
 
 module.exports = {
